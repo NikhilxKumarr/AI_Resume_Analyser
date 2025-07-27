ae355c41a6dd38d35c7c1ee130d3e874d1a5619b
NikhilxKumarr <nk047647@gmail.com>
2025-07-27 15:18:54 +0530

    commit3

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,61 @@
 import type { Route } from "./+types/home";
-import Navbar from '../Components/Navbar';
-import {callback} from 'fdir/dist/api/async';
-import ResumeCard from "../Components/ResumeCard";
-
+import Navbar from "~/Components/Navbar";
+import ScoreCircle from "~/Components/ScoreCircle";
 
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVision" },
+    { name: "description", content: "Smart Feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const resumeImages = [
+    { src: "/images/resume_01.png", company: "Google", job: "Software Engineer", score: 85 },
+    { src: "/images/resume_02.png", company: "Amazon", job: "Backend Developer", score: 70 },
+    { src: "/images/resume_03.png", company: "Microsoft", job: "ML Engineer", score: 92 },
+  ];
+
+
+  return (
+      <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen">
+        <Navbar />
+
+        <section className="main-section">
+          <div className="page-heading text-center py-8">
+            <h1 className="text-3xl font-bold">
+              Track Your Applications & Resume Ratings
+            </h1>
+            <h2 className="text-lg text-gray-600">
+              Review your submissions and AI-powered feedback
+            </h2>
+          </div>
+
+          <div className="resumes-section flex justify-center items-center gap-6 flex-wrap px-4">
+            {resumeImages.map((resume, index) => (
+                <div
+                    key={index}
+                    className="resume-card bg-white shadow-lg p-4 rounded-lg max-w-sm"
+                >
+                  <img
+                      src={resume.src}
+                      alt={`Resume ${index + 1}`}
+                      className="w-full h-[400px] object-cover object-top rounded"
+                  />
+
+                  <div className="mt-4 flex items-center justify-between">
+                    <div>
+                      <h2 className="text-xl font-semibold">{resume.company}</h2>
+                      <p className="text-gray-500">{resume.job}</p>
+                    </div>
+                    <ScoreCircle score={resume.score} />
+                  </div>
+                </div>
+            ))}
+          </div>
+
+        </section>
+      </main>
+  );
 }
