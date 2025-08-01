e28993152307466b7fdd605ee0d58d8eecc4a1d9
NikhilxKumarr <nk047647@gmail.com>
2025-08-01 14:39:11 +0530

    commit3

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -26,6 +26,7 @@ const Upload = () => {
         const uploadedFile = await fs.upload([file]);
         if(!uploadedFile) return setStatusText('Error: Failed to upload file');
 
+
         setStatusText('Converting to image...');
         const imageFile = await convertPdfToImage(file);
         if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image');
